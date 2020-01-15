984c90b03fb9b0dd28ad3578b87806a058d995c8
Nick Burgan-Illig <nick.burgan-illig@puppet.com>
2020-01-15 00:30:42 +0000

    (PE-27189) Change default for repo to 2019.3
    This was pointing to 2018.1 by default for the path to find the repo on enterprise.delivery.puppetlabs.net, which is not going to contain the pe-java-razor package. This changes the default to 2019.3.

--- configs/projects/pe-razor-server.rb
+++ configs/projects/pe-razor-server.rb
@@ -7,7 +7,7 @@ project 'pe-razor-server' do |proj|
 
   proj.setting(:pe_package, true)
   proj.setting(:razor_user, 'pe-razor')
-  proj.setting(:pe_version, ENV['PE_VER'] || '2018.1')
+  proj.setting(:pe_version, ENV['PE_VER'] || '2019.3')
   platform.add_build_repository "http://enterprise.delivery.puppetlabs.net/#{proj.pe_version}/repos/#{platform.name}/#{platform.name}.repo"
 
   proj.instance_eval File.read('configs/projects/razor-server-shared.rb')
